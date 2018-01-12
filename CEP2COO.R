505e54c3b4ae299b981d78081d20245ccd81a599
Thiago Pires <thop100@hotmail.com>
2018-01-12 15:30:53 -0200

    No important change

--- CEP2COO.R
+++ CEP2COO.R
@@ -16,7 +16,7 @@ address_ = try(paste(pycep$consultar_cep(cep)$end,
                      pycep$consultar_cep(cep)$bairro,
                      pycep$consultar_cep(cep)$cidade, "Brasil"), silent = TRUE)
 
-address = ifelse(is.error(address_) == T, 999, address_) # Caso CEP inválido (999)
+address = ifelse(is.error(address_) == T, 999, address_) # Caso CEP invalido (999)
 
 # define geocoding google api url
 geo_url = "https://maps.googleapis.com/maps/api/geocode/xml?address="
