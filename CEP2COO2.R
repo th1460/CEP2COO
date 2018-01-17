177a071c1f2227d79eea25796cfa41c507da2a52
Thiago Pires <thop100@hotmail.com>
2018-01-17 11:04:38 -0200

    New function

---
+++ CEP2COO2.R
@@ -0,0 +1,27 @@
+library(reticulate)
+library(berryFunctions)
+require(photon)
+
+pycep = import("pycep_correios")
+urllib3 = import("urllib3")
+requests = import("requests")
+
+urllib3$disable_warnings(urllib3$exceptions$InsecureRequestWarning)
+
+cep2coo2 <- function(cep){
+  
+  # Busca do endereço através do CEP
+  address_ = try(paste(pycep$consultar_cep(cep)$end, 
+                       pycep$consultar_cep(cep)$bairro,
+                       pycep$consultar_cep(cep)$cidade, "Brasil"), silent = TRUE) 
+  
+  
+  address = ifelse(is.error(address_) == T, "-", address_) # Caso CEP invalido
+  
+  # Query no OSM
+  coo <- geocode(address, limit = 1)[,c("lat", "lon")]
+  
+  return(coo)
+}
+
+
