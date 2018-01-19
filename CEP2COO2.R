dceb82449dc6fe5ef4f82b470c29d0191d122449
Thiago Pires <thop100@hotmail.com>
2018-01-19 16:12:56 -0200

    Fix bugs

--- CEP2COO2.R
+++ CEP2COO2.R
@@ -12,14 +12,13 @@ cep2coo2 <- function(cep){
   
   # Busca do endereço através do CEP
   address_ = try(paste(pycep$consultar_cep(cep)$end, 
-                       pycep$consultar_cep(cep)$bairro,
                        pycep$consultar_cep(cep)$cidade, "Brasil"), silent = TRUE) 
   
   
   address = ifelse(is.error(address_) == T, "-", address_) # Caso CEP invalido
   
   # Query no OSM
-  coo <- geocode(iconv(address, to = "ASCII//TRANSLIT"), limit = 1)[,c("lat", "lon")]
+  coo <- geocode(iconv(address, from = "UTF-8", to = "ASCII//TRANSLIT"), limit = 1)[,c("lat", "lon")]
   
   return(coo)
 }
